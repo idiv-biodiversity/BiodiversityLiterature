16c7df22f173d4eaa1b403064339de6c37262a2e
dylancraven <dylancraven98403@gmail.com>
2018-09-03 21:42:18 +0200

    Finish beta diversity estimates

--- Scripts/1_Integration_BetaDiversity.R
+++ Scripts/1_Integration_BetaDiversity.R
@@ -10,7 +10,7 @@
 require(dplyr)
 require(reshape2)
 require(betapart)
-require(boot)
+require(rms)
 
 ################
 # DATA         #
@@ -19,7 +19,7 @@ require(boot)
 
 ras<-read.delim("Data/RAs_Topics_analyze.csv",sep=",",header=T,stringsAsFactors = FALSE)
 ras$P_A<-1 # for abundance
-rass<-summarize(group_by(ras, TimeWindow, ResArea_Orig,Most_Probable_Topic), Abundance=sum(P_A))
+rass<-dplyr::summarize(group_by(ras, TimeWindow, ResArea_Orig,Most_Probable_Topic), Abundance=sum(P_A))
 rass$ResArea_Orig<-as.factor(rass$ResArea_Orig)
 
 tw1<-"1990_1995"
@@ -70,7 +70,6 @@ rass4<-select(rass4,-ResArea_Orig)
 rass4.pa<-rass4
 rass4.pa[rass4.pa>=1] <- 1
 
-
 ####################################
 # Calculate beta diversity indices #
 # with and without abundance       #
@@ -84,35 +83,65 @@ tw1.abund.beta.out<-rbind(smean.cl.boot(tw1.abund.beta$sampled.values$beta.RUZ.B
                        smean.cl.boot(tw1.abund.beta$sampled.values$beta.RUZ,B=1000))
 tw1.abund.beta.out<-data.frame(tw1.abund.beta.out)
 tw1.abund.beta.out$Beta_Part<-c("BAL","GRA","RUZ")
-
+tw1.abund.beta.out$TimeWindow<-tw1
 
 tw1.pa.beta<-beta.sample(rass1.pa, index="jaccard",sites=nrow(rass1.pa)*0.50,samples=1000)
 tw1.pa.beta.out<-rbind(smean.cl.boot(tw1.pa.beta$sampled.values$beta.JTU,B=1000),smean.cl.boot(tw1.pa.beta$sampled.values$beta.JNE,B=1000),
          smean.cl.boot(tw1.pa.beta$sampled.values$beta.JAC,B=1000))
 tw1.pa.beta.out<-data.frame(tw1.pa.beta.out)
 tw1.pa.beta.out$Beta_Part<-c("JTU","JNE","JAC")
+tw1.pa.beta.out$TimeWindow<-tw1
 
-# stop here
 # Time window 2
 
-tw1.abund.beta<-beta.sample.abund(rass1, index="ruzicka",sites=nrow(rass1)*0.50, samples=1000)
+tw2.abund.beta<-beta.sample.abund(rass2, index="ruzicka",sites=nrow(rass2)*0.50, samples=1000)
+tw2.abund.beta.out<-rbind(smean.cl.boot(tw2.abund.beta$sampled.values$beta.RUZ.BAL,B=1000),smean.cl.boot(tw2.abund.beta$sampled.values$beta.RUZ.GRA,B=1000),
+                          smean.cl.boot(tw2.abund.beta$sampled.values$beta.RUZ,B=1000))
+tw2.abund.beta.out<-data.frame(tw2.abund.beta.out)
+tw2.abund.beta.out$Beta_Part<-c("BAL","GRA","RUZ")
+tw2.abund.beta.out$TimeWindow<-tw3
 
-tw1.pa.beta<-beta.sample(rass1.pa, index="jaccard",sites=nrow(rass1.pa)*0.50,samples=1000)
+tw2.pa.beta<-beta.sample(rass2.pa, index="jaccard",sites=nrow(rass2.pa)*0.50,samples=1000)
+tw2.pa.beta.out<-rbind(smean.cl.boot(tw2.pa.beta$sampled.values$beta.JTU,B=1000),smean.cl.boot(tw2.pa.beta$sampled.values$beta.JNE,B=1000),
+                       smean.cl.boot(tw2.pa.beta$sampled.values$beta.JAC,B=1000))
+tw2.pa.beta.out<-data.frame(tw2.pa.beta.out)
+tw2.pa.beta.out$Beta_Part<-c("JTU","JNE","JAC")
+tw2.pa.beta.out$TimeWindow<-tw2
+
 
 # Time window 3
 
-tw1.abund.beta<-beta.sample.abund(rass1, index="ruzicka",sites=nrow(rass1)*0.50, samples=1000)
+tw3.abund.beta<-beta.sample.abund(rass3, index="ruzicka",sites=nrow(rass3)*0.50, samples=1000)
+tw3.abund.beta.out<-rbind(smean.cl.boot(tw3.abund.beta$sampled.values$beta.RUZ.BAL,B=1000),smean.cl.boot(tw3.abund.beta$sampled.values$beta.RUZ.GRA,B=1000),
+                          smean.cl.boot(tw3.abund.beta$sampled.values$beta.RUZ,B=1000))
+tw3.abund.beta.out<-data.frame(tw3.abund.beta.out)
+tw3.abund.beta.out$Beta_Part<-c("BAL","GRA","RUZ")
+tw3.abund.beta.out$TimeWindow<-tw3
 
-tw1.pa.beta<-beta.sample(rass1.pa, index="jaccard",sites=nrow(rass1.pa)*0.50,samples=1000)
+tw3.pa.beta<-beta.sample(rass3.pa, index="jaccard",sites=nrow(rass3.pa)*0.50,samples=1000)
+tw3.pa.beta.out<-rbind(smean.cl.boot(tw3.pa.beta$sampled.values$beta.JTU,B=1000),smean.cl.boot(tw3.pa.beta$sampled.values$beta.JNE,B=1000),
+                       smean.cl.boot(tw3.pa.beta$sampled.values$beta.JAC,B=1000))
+tw3.pa.beta.out<-data.frame(tw3.pa.beta.out)
+tw3.pa.beta.out$Beta_Part<-c("JTU","JNE","JAC")
+tw3.pa.beta.out$TimeWindow<-tw3
+
 
 # Time window 4
 
-tw1.abund.beta<-beta.sample.abund(rass1, index="ruzicka",sites=nrow(rass1)*0.50, samples=1000)
+tw4.abund.beta<-beta.sample.abund(rass4, index="ruzicka",sites=nrow(rass4)*0.50, samples=1000)
+tw4.abund.beta.out<-rbind(smean.cl.boot(tw4.abund.beta$sampled.values$beta.RUZ.BAL,B=1000),smean.cl.boot(tw4.abund.beta$sampled.values$beta.RUZ.GRA,B=1000),
+                          smean.cl.boot(tw4.abund.beta$sampled.values$beta.RUZ,B=1000))
+tw4.abund.beta.out<-data.frame(tw4.abund.beta.out)
+tw4.abund.beta.out$Beta_Part<-c("BAL","GRA","RUZ")
+tw4.abund.beta.out$TimeWindow<-tw4
 
-tw1.pa.beta<-beta.sample(rass1.pa, index="jaccard",sites=nrow(rass1.pa)*0.50,samples=1000)
-
-
+tw4.pa.beta<-beta.sample(rass4.pa, index="jaccard",sites=nrow(rass4.pa)*0.50,samples=1000)
+tw4.pa.beta.out<-rbind(smean.cl.boot(tw4.pa.beta$sampled.values$beta.JTU,B=1000),smean.cl.boot(tw4.pa.beta$sampled.values$beta.JNE,B=1000),
+                       smean.cl.boot(tw4.pa.beta$sampled.values$beta.JAC,B=1000))
+tw4.pa.beta.out<-data.frame(tw4.pa.beta.out)
+tw4.pa.beta.out$Beta_Part<-c("JTU","JNE","JAC")
+tw4.pa.beta.out$TimeWindow<-tw4
 
 ###
-betatemp_all<-rbind.data.frame(t1t2.t,t2t3.t,t3t4.t)
-write.table(betatemp_all,"/home/dylan/ownCloud/documents/Integrative_Biodiversity/Data_Code_Analysis/Betatemp_RAstopic_31052016.csv",sep=",",row.names=F)
+betadiv_all<-rbind.data.frame(tw1.abund.beta.out,tw1.pa.beta.out,tw2.abund.beta.out,tw2.pa.beta.out,tw3.abund.beta.out,tw3.pa.beta.out,tw4.abund.beta.out,tw4.pa.beta.out)
+write.table(betadiv_all,"Cleaned_Data/Betadiv_RAstopic.csv",sep=",",row.names=F)
